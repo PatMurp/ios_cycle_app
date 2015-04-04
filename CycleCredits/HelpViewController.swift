270de2861e34108dca0ee1761b584499ab1dda9f
Patrick Murphy <murphypatv@gmail.com>
2015-04-04 11:12:39 +0100

    Add help screen

---
+++ CycleCredits/HelpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HelpViewController.swift
+//  CycleCredits
+//
+//  Created by Patrick Murphy on 04/04/2015.
+//  Copyright (c) 2015 Patrick Murphy. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
