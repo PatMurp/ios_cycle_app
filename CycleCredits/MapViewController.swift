294545459677c7bf686717eca2f8dbe3f3e8ad2d
Patrick Murphy <murphypatv@gmail.com>
2015-04-13 15:40:18 +0100

    centre map on default location and set zoom level

--- CycleCredits/MapViewController.swift
+++ CycleCredits/MapViewController.swift
@@ -16,7 +16,40 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // wit cordinates
+        var latitude:CLLocationDegrees = 52.245748
+        var longitude:CLLocationDegrees = -7.138378
+        
+        // zoom
+        var latDelta:CLLocationDegrees = 0.01
+        var lonDelta:CLLocationDegrees = 0.01
+        
+        // zooom area
+        var theSpan:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        // location
+        var bikeStand:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        // region display
+        var theRegion:MKCoordinateRegion = MKCoordinateRegionMake(bikeStand, theSpan)
+        
+        // display map
+        self.mapView.setRegion(theRegion, animated: true)
+        
+        // pin
+        var bikeStandLocation = MKPointAnnotation()
+        
+        // pin cordinates
+        bikeStandLocation.coordinate = bikeStand
+        
+        // title
+        bikeStandLocation.title = "WIT Bike Stand"
+        
+        // subtitle
+        bikeStandLocation.subtitle = "Journey starts here"
+        
+        // display info and pin
+        self.mapView.addAnnotation(bikeStandLocation)
     }
 
     override func didReceiveMemoryWarning() {
