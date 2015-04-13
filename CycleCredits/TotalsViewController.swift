2f54081110508d0e1c1960b1997c8ec0c0c8dc9b
Patrick Murphy <murphypatv@gmail.com>
2015-04-13 15:01:38 +0100

    calculate total co2 savings

--- CycleCredits/TotalsViewController.swift
+++ CycleCredits/TotalsViewController.swift
@@ -16,6 +16,7 @@ class TotalsViewController: UIViewController {
     
     var valueCc: Double = 7
     var totalDistance = 0.0
+    var totalCo2Savings = 0.0
     
     @IBOutlet weak var tdistance: UITextField!
     @IBOutlet weak var tco2Savings: UITextField!
@@ -23,23 +24,20 @@ class TotalsViewController: UIViewController {
     @IBOutlet weak var ccValueSlider: UISlider!
     @IBOutlet weak var ccValue: UILabel!
     
-    // temp values for testing only
-    var tempco2Savings = 2.3
-   
     // set carbon credit value and recalculate total value when slider is moved
     @IBAction func sliderValueChanged(sender: UISlider) {
         
         var currentValue = lroundf(sender.value) // whole numbers only
         ccValue.text = "\(currentValue)/tonne"
-        tValue.text = String(format: "€%0.02f", calcCo2Value(tempco2Savings, Double(currentValue)))
+        tValue.text = String(format: "€%0.02f", calcCo2Value(self.sumCo2Savings(), Double(currentValue)))
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tdistance.text = String(format: "%0.02f km", self.sumDistance()) // set total distance
-        tco2Savings.text = String(format: "%0.03f kg", tempco2Savings)
-        tValue.text = String(format: "€%0.02f", calcCo2Value(tempco2Savings, valueCc))
+        tco2Savings.text = String(format: "%0.03f kg", self.sumCo2Savings())
+        tValue.text = String(format: "€%0.02f", calcCo2Value(self.sumCo2Savings(), valueCc))
     }
 
     override func didReceiveMemoryWarning() {
@@ -56,6 +54,16 @@ class TotalsViewController: UIViewController {
             totalDistance += r.distance
         }
         return totalDistance
+    }
+    
+    // calculate total co2 savings
+    func sumCo2Savings() -> Double {
+        
+        for route in routes {
+            var r = route as Route
+            totalCo2Savings += (r.distance * emissionForBand(r.co2band)!) / 1000
+        }
+        return totalCo2Savings
     }
 }
 
